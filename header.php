b9750ba2c26101e9bcdaff21358ce34a97b7a69e
danidorado <danidorado@BigVicius.local>
2018-12-11 17:10:52 +0500

    dani changes

--- header.php
+++ header.php
@@ -4,5 +4,5 @@
 	<link rel='stylesheet' href='http://techiereef.com/wp-content/themes/techiereef/css/global_grid.css' type='text/css' />
   <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
 </head>
-<?php echo 'head';?>
+
 <body>
