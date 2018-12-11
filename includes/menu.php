59c814a34bbd3187d9814a017c7b8eb21f464ace
Francisco Roca <frrocaguilar@gmail.com>
2018-12-11 18:50:38 +0500

    changes

--- includes/menu.php
+++ includes/menu.php
@@ -4,11 +4,12 @@
 		
 	</div>
 	<div class="particles">particles</div>
-	<div class="photo"><svg id="icon-close" viewBox="0 0 119 25" width="100%" height="100%">	  		
-		<path d="M36,8 L36,0 L100,0 L100,8 L36,8 Z M0,8 L0,0 L24,0 L24,8 L0,8 Z M0,28 L0,20 L71,20 L71,28 L0,28 Z"></path>
-		</svg></div>
-	
-	<nav class="navigation">
+	<div class="photo">
+        <svg id="icon-close" viewBox="0 0 119 25" width="100%" height="100%">	  		
+		    <path d="M36,8 L36,0 L100,0 L100,8 L36,8 Z M0,8 L0,0 L24,0 L24,8 L0,8 Z M0,28 L0,20 L71,20 L71,28 L0,28 Z"></path>
+        </svg>
+    </div>
+    <nav class="navigation">
         <ul>
             <li><a href="/home">home</a></li>
             <li><a href="/portfolio">portfolio</a></li>
