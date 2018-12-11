58ec8a8b42aaa9355f3e9b1c2a310a0f6ce9958d
Francisco Roca <frrocaguilar@gmail.com>
2018-12-11 17:26:32 +0500

    changes

--- home.php
+++ home.php
@@ -8,29 +8,25 @@
  */
 ?>
 <!DOCTYPE html>
-<html <?php language_attributes(); ?> class="no-js">
+<html <?php //language_attributes(); ?> class="no-js">
 <head>
 	<meta name="google-site-verification" content="hx9xBRXoplTZvwaskvY-mHP0nQOPPnpLpPnkgJx5ONc" />
 	<title>TechieReef - Digital Strategies</title>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta charset="<?php //bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	
 	<!--[if lt IE 9]>
-	<script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/html5.js"></script>
+	<script src="<?php //echo esc_url( get_template_directory_uri() ); ?>/js/html5.js"></script>
 	<![endif]-->
 
-	<?php wp_head(); ?>
+	<?php //wp_head(); ?>
 	
-	
-	
-	<link rel='stylesheet' href='/wp-content/themes/techiereef/css/global_grid.css' type='text/css' />
-
-	
-	<script src="/wp-content/themes/techiereef/js/particles.js"></script>
-		
+	<link rel="stylesheet" href="css/global_grid.css" type="text/css"/>
+	<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+	<script src="js/particles.js"></script>
 </head>
 
-<body <?php body_class(); ?>>
+<body <?php //body_class(); ?>>
 	
 
 <div class="grid-container">
