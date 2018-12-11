ee5ea5a4cee76785a0c2a554817f700491ddbd7f
Francisco Roca <frrocaguilar@gmail.com>
2018-12-11 18:11:22 +0500

    changes

--- home.php
+++ home.php
@@ -30,9 +30,8 @@
 </head>
 
 <body <?php //body_class(); ?>>
-	
 
-<div class="grid-container">
+<div id="home-grid">
   <div class="logo">
     <img src="<?php $url ?>assets/images/techiereef-logo.png"/>
   </div>
@@ -43,58 +42,45 @@
     </svg>
   </div>
   <div class="slogan-container">
-    
-        <h1>creating digital solutions</h1>
-    
+    <h1>creating digital solutions</h1>
   </div>  
   <div class="button">
-	  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+	<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
 </div>
 </div>
 	
-  	<div id="menu-grid">
-	  	<div class="portfolio">portfolio</div>
-	  	<div class="video">
-		  	
-    	</div>
-		<div class="particles">particles</div>
-		<div class="photo"><svg id="icon-close" viewBox="0 0 119 25" width="100%" height="100%">	  		
-	  		<path d="M36,8 L36,0 L100,0 L100,8 L36,8 Z M0,8 L0,0 L24,0 L24,8 L0,8 Z M0,28 L0,20 L71,20 L71,28 L0,28 Z"></path>
-    		</svg></div>
-		
-		<nav class="navigation">navigation</nav>  	
-	</div>
-
+<?php include('includes/menu.php') ?>
     
-	
-	<script>
-	    jQuery(document).ready(function(){
-			jQuery('#icon-menu').click(function(){
-				openMenu();
-			});
-			jQuery('#icon-close').click(function(){
-				closeMenu();
-			});
+<script>
+	jQuery(document).ready(function(){
+
+		// OPEN MENU
+		jQuery('#icon-menu').click(function(){
+			openMenu();
 		});
-		
-		function openMenu(){
-			jQuery('#menu-grid').addClass('active');
-			setTimeout(()=> {
-				jQuery('#menu-grid').addClass('move');
-			});
-		}
-	    
-		function closeMenu(){
-			
-			jQuery('#menu-grid').removeClass('move');
 
-				setTimeout(
-					()=> {jQuery('#menu-grid').removeClass('active');
+		// CLOSE MENU
+		jQuery('#icon-close').click(function(){
+			closeMenu();
+		});
 
-			}, 500);
-		}
+	});
+	
+	function openMenu(){
+		jQuery('#menu-grid').addClass('active');
+		setTimeout(()=> {
+			jQuery('#menu-grid').addClass('move');
+		});
+	}
+	
+	function closeMenu(){
+		jQuery('#menu-grid').removeClass('move');
+		setTimeout(()=> {
+			jQuery('#menu-grid').removeClass('active');
+		}, 500);
+	}
 
-	</script>
+</script>
 			
 </body>
 </html>
