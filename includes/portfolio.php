8538472e3e7e528f7fe6dc5b407c17909501e824
Francisco Roca <frrocaguilar@gmail.com>
2018-12-15 17:43:57 +0500

    css changes

--- includes/portfolio.php
+++ includes/portfolio.php
@@ -14,7 +14,7 @@
 <html class="no-js">
 <head>
 	<meta name="google-site-verification" content="hx9xBRXoplTZvwaskvY-mHP0nQOPPnpLpPnkgJx5ONc" />
-	<title>TechieReef - Digital Strategies</title>
+	<title>TechieReef - Portfolio</title>
 	<meta charset="<?php //bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	
@@ -30,25 +30,8 @@
 	<script src="js/particles.js"></script>
 </head>
 <body>
-<div id="hero-grid">
-	<div class="logo">
-		<a href="/">
-			<img src="<?php $url ?>assets/images/techiereef-logo.png"/>
-		</a>
-	</div>
-	<div class="menu">
-		<svg id="icon-menu" viewBox="0 0 119 25" width="100%" height="100%">
-			<title>techireef logo</title>
-			<path class="hover-menu-color" d="M36,8 L36,0 L100,0 L100,8 L36,8 Z M0,8 L0,0 L24,0 L24,8 L0,8 Z M0,28 L0,20 L71,20 L71,28 L0,28 Z"></path>
-		</svg>
-	</div>
-	<div class="slogan-container">
-		<h1>creating digital solutions</h1>
-		<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-	</div>  
-	<div class="button">
-		
-	</div>
+<div id="portfolio-grid">
+	
 </div>
 <?php include('includes/menu.php'); ?>
 <?php include('includes/values.php'); ?>	
--- includes/values.php
+++ includes/values.php
@@ -1,11 +1,17 @@
-<?php
-?>
-
 <div id="values-grid">
-    <div class="values-header"></div>
-    <div class="big-picture"></div>
-    <div class="small-picture"></div>
-    <div class="values-content"></div>
-    <div class="interaction"></div>
-    <div class="relleno"></div>
+    <div class="values">
+        <h1>Hope</h1>
+        <div class="hope-definition">
+            <p>This is the denitiion of hope</p>
+        </div>
+        <div class="values-navigation">
+            <ul>
+                <li><</li>
+                <li>></li>
+            </ul>
+        </div>
+    </div>
+    <div class="value-img">
+    
+    </div>
 </div>
